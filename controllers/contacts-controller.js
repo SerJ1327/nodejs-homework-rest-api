f0f86aa20ee6fca36059d04d4f7185d8370a5e93
Serhii Podhornyi <serhii.podhornyi@gmail.com>
2023-09-16 22:38:55 +0300

    task2_node.js

---
+++ controllers/contacts-controller.js
@@ -0,0 +1,48 @@
+import contactsService from "../models/contacts/contacts.js";
+import { ctrlWrapper } from "../decorators/index.js";
+import { HttpError } from "../helpers/index.js";
+
+const getAll = async (req, res) => {
+  const result = await contactsService.listContacts();
+  res.json(result);
+};
+
+const getById = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await contactsService.getContactById(contactId);
+  if (!result) {
+    throw HttpError(404, `Contact with id=${contactId} not found`);
+  }
+  res.json(result);
+};
+
+const add = async (req, res) => {
+  const result = await contactsService.addContact(req.body);
+  res.status(201).json(result);
+};
+
+const updateById = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await contactsService.updateContactById(contactId, req.body);
+  if (!result) {
+    throw HttpError(404, `Contact with id=${contactId} not found`);
+  }
+  res.json(result);
+};
+
+const deleteById = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await contactsService.removeContact(contactId, req.body);
+  if (!result) {
+    throw HttpError(404, `Contact with id=${contactId} not found`);
+  }
+  res.status(204).send();
+};
+
+export default {
+  getAll: ctrlWrapper(getAll),
+  getById: ctrlWrapper(getById),
+  add: ctrlWrapper(add),
+  updateById: ctrlWrapper(updateById),
+  deleteById: ctrlWrapper(deleteById),
+};
